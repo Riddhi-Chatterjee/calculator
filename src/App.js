b5248ce190320857f14b66d6345b3cd7fa3e4e51
Riddhi-Chatterjee <riddhich@rediffmail.com>
2023-10-28 23:40:13 +0530

    adding buttons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import Wrapper from './components/Wrapper';
-import Screen from './components/Screen';
+import Wrapper from './Wrapper';
+import Screen from './Screen';
+
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "x"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="]
+];
 
 function App() {
   return (
