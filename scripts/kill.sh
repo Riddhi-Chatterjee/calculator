2f5d5e26b041e676020aa367b529dfb9b041340f
Riddhi-Chatterjee <riddhich@rediffmail.com>
2023-10-29 12:34:40 +0530

    bug fixes

--- scripts/kill.sh
+++ scripts/kill.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env sh
 
-echo 'This script obtains the PID of the "npm start" process and kills it,'
+echo 'This script stops the docker container in which we were running our app,'
 echo 'thereby shutting down our calculator application running on localhost:3000'
 set -x
-lsof -i :3000 >> temp.txt
-pid_3000=$(awk 'NR == 2 {print $2}' temp.txt)
-kill $pid_3000
-rm temp.txt
+/usr/local/bin/docker stop $(cat ./container_id.txt)
+rm ./container_id.txt
